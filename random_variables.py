2929f67c8381aa5a988be5eb3dd2de5851bbf0e6
JaredMijailRE <jramirezes@unal.edu.co>
2025-04-25 00:09:57 -0500

    segunda grafica metodo de la inversa

--- random_variables.py
+++ random_variables.py
@@ -26,9 +26,30 @@ result = simulation(100000000, lambda: inverse_cdf_triangular(a, b, c))
 print("Simulación de 100,000,000 muestras de la distribución triangular:")
 print(result[:10]) 
 
-plt.hist(result, bins=1000, density=True)
-plt.title("Distribución Triangular: Simulada vs Teórica")
-plt.xlabel("x")
-plt.ylabel("Densidad")
+def makeGraph(result):
+    plt.hist(result, bins=1000, density=True)
+    plt.title("Distribución Triangular: Simulada vs Teórica")
+    plt.xlabel("x")
+    plt.ylabel("Densidad")
 
-plt.show()
+    plt.show()
+
+@jit(nopython=True)
+def inverse_cdf_trapezoidal( a, b, c, d):
+    u = np.random.random()
+    
+    h = 2 / (d + c - a - b)
+
+    A1 = (b - a) * h / 2
+    A2 = (c - b) * h
+    A3 = (d - c) * h / 2
+
+    if u < A1:
+        return a + np.sqrt((2 * u * (b - a)) / h)
+    elif u < A1 + A2:
+        return b + (u - A1) / h
+    elif u <= 1.0:
+        z = u - A1 - A2
+        return d - np.sqrt((2 * (A3 - z) * (d - c)) / h)
+    else:
+        raise ValueError("u debe estar en el intervalo [0, 1]")
